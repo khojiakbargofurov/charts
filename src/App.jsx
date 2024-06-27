aaca0ba322a529d229032850067d59e6a2265ea5
Khojiakbar <harry.gafurov@gmail.com>
2024-06-27 14:25:52 +0500

    charts

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   
   return (
     <>
-      <PieChart/>
       <BarChart/>
+      <PieChart/>
     </>
   )
 }
--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,49 +1,136 @@
-import { useEffect, useState } from "react"
-import ReactApexChart from "react-apexcharts"
+import React, { useEffect } from "react";
+import { useState } from "react";
+import ReactApexChart from "react-apexcharts";
 
 function BarChart() {
-  useEffect(()=> {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((data) => data.json())
-      .then((countries) => {})
-  })
-  const [data, setData] = useState({       
-    series: [{
-      data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
-    }],
+
+  const [data2, setData2] = useState({
+    series: [
+      {
+        data: [],
+      },
+    ],
     options: {
       chart: {
-        type: 'bar',
-        height: 350
+        type: "bar",
+        height: 380,
       },
       plotOptions: {
         bar: {
           borderRadius: 4,
-          borderRadiusApplication: 'end',
+          borderRadiusApplication: "end",
           horizontal: true,
-        }
+        },
       },
-      dataLabels: {
-        enabled: false
+      colors: [
+        "#33b2df",
+        "#546E7A",
+        "#d4526e",
+        "#13d8aa",
+        "#A5978B",
+        "#2b908f",
+        "#f9a3a4",
+        "#90ee7e",
+        "#f48024",
+        "#69d2e7",
+      ],
+      yaxis: {
+        reversed: true,
+        axisTicks: {
+          show: true,
+        },
       },
       xaxis: {
-        categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
-          'United States', 'China', 'Germany'
-        ],
-      }
+        categories: [],
+      },
     },
-  
-  
-  })
+  });
+  const [data, setData] = useState({
+    series: [
+      {
+        data: [],
+      },
+    ],
+    options: {
+      chart: {
+        type: "bar",
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 4,
+          borderRadiusApplication: "end",
+          horizontal: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
 
+      xaxis: {
+        categories: [],
+      },
+    },
+  });
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((data) => data.json())
+      .then((countires) => {
+        const names = countires
+          .sort((a, b) => b.area - a.area)
+          .map((country) => {
+            return country.name.common;
+          });
+        const names2 = countires
+          .sort((a, b) => b.population - a.population)
+          .map((country) => {
+            return country.name.common;
+          });
+        const populitions = countires
+          .sort((a, b) => b.population - a.population)
+          .map((country) => {
+            return country.population;
+          });
+        const areas = countires
+          .sort((a, b) => b.area - a.area)
+          .map((country) => {
+            return country.area;
+          });
+        setData2({
+          series: [{ data: populitions }],
+          options: {
+            ...data.options,
+            xaxis: {
+              categories: names2,
+            },
+          },
+        });
+        setData({
+          series: [{ data: areas }],
+          options: {
+            ...data.options,
+            xaxis: {
+              categories: names,
+            },
+          },
+        });
+
+              });
+  }, []);
   return (
-    <div>
-      <div id="chart">
-        <ReactApexChart options={data.options} series={data.series} type="bar" height={350} />
+         <div>
+        <h1 className="name">Aholisi</h1>
+        <div id="chart">
+          <ReactApexChart
+            series={data2.series}
+            options={data2.options}
+            type="bar"
+            height={3100}
+            width={1200}
+          />
         </div>
-      <div id="html-dist"></div>
-    </div>
-  )
+      </div>
+          );
 }
 
-export default BarChart
+export default BarChart;
